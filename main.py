581e8ae0e00cbf9fa4593f0e61fe2cff6cb894b7
marleyabo <159666743+marleyabo@users.noreply.github.com>
2024-04-19 18:47:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def play():
     print("start:", startTime)
     # You can perform any necessary processing here
 
-    return render_template('templates/play.html', videoUrl=videoUrl, startTime=startTime)
+    return render_template('play.html', videoUrl=videoUrl, startTime=startTime)
 
 @app.route('/execute_tester', methods=['GET'])
 def execute_tester():
@@ -36,7 +36,7 @@ def execute_tester():
         # Extract data from the remaining columns (excluding column A)
         data = [[ws.cell_value(row, col) for col in range(1, ws.ncols)] for row in range(ws.nrows)]
         # Pass the timestamps and the entire data to the HTML template
-        return render_template('templates/results.html', results=results, dateString=dateString, timestamps=timestamps, data=data, get_video_url_and_start_time=get_video_url_and_start_time)
+        return render_template('results.html', results=results, dateString=dateString, timestamps=timestamps, data=data, get_video_url_and_start_time=get_video_url_and_start_time)
     else:
         return "Invalid Request: Missing dateString"
 
